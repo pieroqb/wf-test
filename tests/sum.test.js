669a8ddc15f59f1e8f70a34f767456416eb6d8e4
= <pieroq.2899@gmail.com>
2022-05-08 22:54:07 -0500

    add fail test 9999

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -17,3 +17,7 @@ test('adds 1 + 2 to equal 3', () => {
   test('adds 2 + 2 to equal 10', () => {
     expect(2+2).toBe(10);
   });
+
+  test('adds 2 + 2 to equal 11', () => {
+    expect(2+2).toBe(11);
+  });
