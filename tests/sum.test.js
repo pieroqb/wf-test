58c51fefeb5b9c5d592d8ca95b3ae4b85dbb0cd1
= <pieroq.2899@gmail.com>
2022-05-08 21:55:49 -0500

    fix test 2

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -6,6 +6,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(1+1).toBe(2);
   });
 
-  test('adds 2 + 2 to equal 5', () => {
-    expect(2+2).toBe(5);
+  test('adds 2 + 2 to equal 4', () => {
+    expect(2+2).toBe(4);
   });
