c489a8a8ba3ddce57b76da2d929ee0a0d375d058
= <pieroq.2899@gmail.com>
2022-05-08 22:09:53 -0500

    fail again

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -6,6 +6,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(1+1).toBe(2);
   });
 
-  test('adds 2 + 2 to equal 4', () => {
-    expect(2+2).toBe(4);
+  test('adds 2 + 2 to equal 2', () => {
+    expect(2+2).toBe(2);
   });
